03b1b057d8c646c6e385c4818de898b365a174e2
dkusnieruk <dariusz.kusnieruk@wp.pl>
2023-05-10 06:26:31 +0200

    update task

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,43 +1,24 @@
-import { useWallet } from '../../hooks';
-import Media from 'react-media';
+import { useEffect } from 'react';
+import { useAuth, useWallet } from '../../hooks';
 import scss from './Balance.module.scss';
-import { AddButton } from '../utils/AddButton/AddButton';
+import { useDispatch } from 'react-redux';
+import { getTransactions } from '../../redux/wallet/walletThunk';
+
 export const Balance = () => {
   const { balance } = useWallet();
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    dispatch(getTransactions({ walletId: user.wallets[0].id }));
+  }, [dispatch, user.wallets]);
 
   return (
-   <Media
-   queries={{
-    mob: '(max-width: 767px)',
-    tab: '(min-width: 768px) and (max-width: 1280px)',
-    desk: '(min-width: 1280px)',
-  }}>
-    {matches =>(
-      <>
-      {matches.mob ? (
-           <div className={scss.balance}>
+    <div className={scss.balance}>
       <div className={scss.balanceMain}>
         <div className={scss.balanceTitle}>Your Balance</div>
         <div className={scss.balanceNumber}>{balance}</div>
       </div>
-      <AddButton/>
     </div>
-      ) :(<div className={scss.balance}>
-        <div className={scss.balanceMain}>
-          <div className={scss.balanceTitle}>Your Balance</div>
-          <div className={scss.balanceNumber}>{balance}</div>
-        </div>
-      </div>)}
-      </>
-    )}
-   </Media>
-
-
-    // <div className={scss.balance}>
-    //   <div className={scss.balanceMain}>
-    //     <div className={scss.balanceTitle}>Your Balance</div>
-    //     <div className={scss.balanceNumber}>{balance}</div>
-    //   </div>
-    // </div>
   );
 };
