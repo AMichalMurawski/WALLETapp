b2103253df3ac4a1e52167f7fb9f8103bfc5c825
dkusnieruk <dariusz.kusnieruk@wp.pl>
2023-05-10 07:03:09 +0200

    update Balance
    improved

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -3,22 +3,42 @@ import { useAuth, useWallet } from '../../hooks';
 import scss from './Balance.module.scss';
 import { useDispatch } from 'react-redux';
 import { getTransactions } from '../../redux/wallet/walletThunk';
+import Media from 'react-media';
+import { AddButton } from '../utils/AddButton/AddButton';
 
 export const Balance = () => {
-  const { balance } = useWallet();
-  const dispatch = useDispatch();
-  const { user } = useAuth();
+ const { balance } = useWallet();
+ const dispatch = useDispatch();
+ const { user } = useAuth();
+ useEffect(() => {
+   dispatch(getTransactions({ walletId: user.wallets[0].id }));
+ }, [dispatch, user.wallets]);
 
-  useEffect(() => {
-    dispatch(getTransactions({ walletId: user.wallets[0].id }));
-  }, [dispatch, user.wallets]);
-
-  return (
-    <div className={scss.balance}>
-      <div className={scss.balanceMain}>
-        <div className={scss.balanceTitle}>Your Balance</div>
-        <div className={scss.balanceNumber}>{balance}</div>
-      </div>
+ return (
+  <Media
+  queries={{
+  mob: '(max-width: 767px)',
+  tab: '(min-width: 768px) and (max-width: 1280px)',
+  desk: '(min-width: 1280px)',
+ }}>
+  {matches =>(
+   <>
+   {matches.mob ? (
+      <div className={scss.balance}>
+   <div className={scss.balanceMain}>
+    <div className={scss.balanceTitle}>Your Balance</div>
+    <div className={scss.balanceNumber}>{balance}</div>
+   </div>
+   <AddButton/>
+  </div>
+   ) :(<div className={scss.balance}>
+    <div className={scss.balanceMain}>
+     <div className={scss.balanceTitle}>Your Balance</div>
+     <div className={scss.balanceNumber}>{balance}</div>
     </div>
-  );
+   </div>)}
+   </>
+  )}
+  </Media>
+ );
 };
